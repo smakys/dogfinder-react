9c7c05580deb8e0eb92c1706e0e749bcb7e55161
Sui Mak <sui.mak@zaplabs.com>
2018-09-21 11:10:17 -0700

    add actions constants

---
+++ src/actions.js
@@ -0,0 +1,1 @@
+export const CHANGE_ANIMAL = 'CHANGE_ANIMAL';
--- src/reducers/animal.js
+++ src/reducers/animal.js
@@ -1,6 +1,8 @@
+import { CHANGE_ANIMAL } from '../actions';
+
 export default function animalReducer(state = '', action) {
   switch (action.type) {
-    case 'SET_ANIMAL':
+    case CHANGE_ANIMAL:
       return action.payload;
     default:
       return state;
